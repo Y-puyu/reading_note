cfa0c4b601381049ae126d95b797b7bbf2db32d6
Y-puyu <345462072@qq.com>
2020-12-05 22:49:16 +0800

    C和指针 ch1

---
+++ Pointers On C/src/ch1/README.md
@@ -0,0 +1,10 @@
+# 第一章：快速上手
+
+简单看了看 `printf` 的格式化和骚操作，顺手找了两个官方文档：
+
+- C++ 文档：[http://www.cplusplus.com/](http://www.cplusplus.com/reference/cstdio/printf/?kw=printf)
+- C 文档，包含有 C++：[https://en.cppreference.com/](https://en.cppreference.com/mwiki/index.php?title=Special%3ASearch&search=printf&button=)
+
+`printf` 的返回值是输出到屏幕的字符个数，故可以嵌套 `printf` 来输出一个整数的位数，很是秀...
+
+`printf` 可以
---
+++ Pointers On C/src/ch1/demo01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    printf("%d\n", printf("%d\n", a));    // 想得到的骚操作之一
+
+    printf("\033[1m\033[45;33m HELLO_WORLD \033[0m\n");     // 粉色底色，黄字的 HELLO_WORLD
+    
+    // 有关格式化
+    int b = 4;
+    float c;
+    double d;
+    long e;
+    long long f;
+    unsigned long long g;
+    return 0;
+}
