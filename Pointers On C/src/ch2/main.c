a09367accbe4671aee99daac57e468702a6323b9
Y-puyu <345462072@qq.com>
2020-12-13 20:14:34 +0800

    C和指针 update ch2 单独编译即“”头文件

--- Pointers On C/src/ch2/main.c
+++ Pointers On C/src/ch2/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "increment.c"      // 这样也可以
+#include "negate.c"        // " "是在整个系统下寻找./m该文件
 
 int main( void )
 {
@@ -7,4 +9,3 @@ int main( void )
     printf( "%d %d\n", increment(-10), negate(-10));
     return 0;
 }
-
